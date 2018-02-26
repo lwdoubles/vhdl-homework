124531b8e2f49655d3785a6e00a0de54c8ef44d7
lwdoubles <985341219@qq.com>
2018-02-26 18:09:26 +0800

    Update ram_model.vhd

--- ram_model.vhd
+++ ram_model.vhd
@@ -17,23 +17,26 @@ end ram_model;
 
 architecture rtl of ram_model is
 	type ram_type is array(0 to 7) of bit16;
-	signal temp_data:bit16;
+	signal temp_data1,temp_data2:bit16;
 begin
-	process(clk,sel)
+	process(clk,sel,reg_wr)
 		variable ramdata:ram_type;
 	begin
 		if reg_wr = '1' then 
 		  ramdata(conv_integer(sel(5 downto 3))) := input_data after 10 s;
-		  reg_rd <= '0';
+		  reg_wr <= '0';
 		end if;
-		temp_data1 <= ramdata(conv_integer(sel(5 downto 3))	after 1 s;
+		
 	end process;
 	
-	process(temp_data1)
+	process(temp_data1,temp_data2,reg_rd)
 	begin
 		if reg_rd = '1' then
-			qout1 <= temp_data after 1 ns;
-			reg_wr <= '0';
+			temp_data1 <= ramdata(conv_integer(sel(5 downto 3))	after 1 s;
+			temp_data2 <= ramdata(conv_integer(sel(2 downto 0))	after 1 s;
+			qout1 <= temp_data1 after 1 ns;
+			qout2 <= temp_data2 after 1 ns;
+			reg_rd <= '0';
 	end process;
 	
 end rtl;
