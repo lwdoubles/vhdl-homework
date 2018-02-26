0065849870ea486db30010c3064d1971c2ec7232
lwdoubles <985341219@qq.com>
2018-02-26 18:49:50 +0800

    Update ram_model.vhd

--- ram_model.vhd
+++ ram_model.vhd
@@ -10,33 +10,28 @@ entity ram_model is
 	     		reg_rd:in std_logic;
 			en:in std_logic;
 			clk:in std_logic;
-			qout1: out bit16
-	    		qout2: out bit16);
+			qout: out bit16);
 			
 end ram_model;
 
 architecture rtl of ram_model is
 	type ram_type is array(0 to 7) of bit16;
-	signal temp_data1,temp_data2:bit16;
+	signal temp_data:bit16;
 begin
-	process(clk,sel,reg_wr)
+	process(clk,sel)
 		variable ramdata:ram_type;
 	begin
-		if reg_wr = '1' then 
-		  ramdata(conv_integer(sel(5 downto 3))) := input_data after 10 s;
-		  reg_wr <= '0';
+		if clk'event and clk = '1' then 
+		  ramdata(conv_integer(sel)) := input_data;
 		end if;
-		
+		temp_data <= ramdata(conv_integer(sel)) after 1 ns;
 	end process;
 	
 	process(temp_data1,temp_data2,reg_rd)
 	begin
-		if reg_rd = '1' then
-			temp_data1 <= ramdata(conv_integer(sel(5 downto 3))	after 1 s;
-			temp_data2 <= ramdata(conv_integer(sel(2 downto 0))	after 1 s;
-			qout1 <= temp_data1 after 1 ns;
-			qout2 <= temp_data2 after 1 ns;
-			reg_rd <= '0';
+		if en = '1' then
+			qout <= temp_data after 1 ns;
+		end if;
 	end process;
 	
 end rtl;
