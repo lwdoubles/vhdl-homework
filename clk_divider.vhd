a5429ca56de759f9eb8455536d171d8886030b4b
a8228771 <31787088+a8228771@users.noreply.github.com>
2018-02-28 19:03:58 +0800

    Update clk_divider.vhd

--- clk_divider.vhd
+++ clk_divider.vhd
@@ -3,7 +3,7 @@ USE IEEE.std_logic_1164.ALL;
 ENTITY clk_divider IS
 PORT(clk,rst:IN std_logic;
 	  CLOCK:OUT std_logic);
-END divider;
+END clk_divider;
 ARCHITECTURE behav OF clk_divider IS
 SIGNAL count :INTEGER RANGE 0 TO 49999999;
 BEGIN
