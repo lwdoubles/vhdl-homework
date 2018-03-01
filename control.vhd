4018e9f7b4b25c259f8c1e93f7cf3a2ba0891403
lwdoubles <985341219@qq.com>
2018-03-01 19:50:59 +0800

    Add files via upload

--- control.vhd
+++ control.vhd
@@ -18,11 +18,12 @@ port(clock:in std_logic;
 	 outRegRd:out std_logic;
 	 --shiftSel:out shift_type;
 	 aluSel:out alu_type;
-	 compSel:out comp_type;
+	 --compSel:out comp_type;
 	 opRegRd:out std_logic;
 	 opRegWr:out std_logic;
 	 instrWr:out std_logic;
-	 regSel:out reg_type;
+	 regSel:buffer reg_type;
+	 outsel:out std_logic;
 	 regRd:out std_logic;
 	 regWr:out std_logic;
 	 rw:out std_logic;
@@ -42,13 +43,17 @@ begin
 	 addrRegWr <= '0';
 	 outRegWr <= '0';
 	 outRegRd <= '0';
+	 outsel <= '0';
+	 --next_state <= execute;
 	 --shiftSel <= shiftpass;
-	 --aluSel <= alupass;
+	 aluSel <= "0000";
 	 --compSel <= eq;
 	 opRegRd <= '0';
 	 opRegWr <= '0';
 	 instrWr <= '0';
-	 regSel <= "000";
+	 --if regSel = "XXX" then	
+		--regSel <= "000";
+	 --end if;
 	 regRd <= '0';
 	 regWr <= '0';
 	 rw <= '0';
@@ -67,49 +72,103 @@ begin
 				--add op	
 				when "00010" =>
 					next_state <= add1;
+					
+				when "10010" =>
+					next_state <= sub1;
 				
 				when others =>
 					next_state <= incPc;
 			end case;
-		
-			
-				
-	 	
+		 	
 		when load1 =>
+			regRd <= '0';
 			regWr <= '1';
 			OpRegWr <= '1';
 			regSel <= instrReg(2 downto 0);	
 			next_state <= load2;
 			
 		when load2 =>
-			OpRegWr <= '0';
+			regRd <= '0';
 			regWr <= '1';
-			next_state <= load3;
-			
-		when load3 =>
-			OpRegWr <= '0';
-			regWr <= '1';
-			regSel <= instrReg(5 downto 3);
-			next_state <= incPc;
-		
-			
-		when add1 => 
-			aluSel <= alu_add;							
-			regSel <= instrReg (5 downto 3);
+			OpRegWr <= '1';
+			regSel <= instrReg(2 downto 0);	
+			next_state <= move1;
+							
+		when move1 =>
 			regRd <= '1';
+			regWr <= '0';
+			next_state <= move2;
+			
+		when move2 =>
+			regRd <= '1';
+			regWr <= '0';
 			next_state <= incPc;
+			
+		when move3 =>
+			regRd <= '1';
+			regWr <= '0';
+			outsel <= '1';
+			next_state <= move4;
+			
+		when move4 =>
+			regRd <= '1';
+			regWr <= '0';
+			outsel <= '1';
+			next_state <= move5;
+		
+		when move5 =>
+			outSel <= '0';
+			next_state <= incPc;
+			
+		
+						
+		when add1 => 
+			OpRegWr <= '0';
+			aluSel <= alu_add;
+			--outSel <= 							
+			regRd <= '0';
+			regWr <= '1';
+			regSel <= instrReg(2 downto 0);	
+			next_state <= add2;
+			
+		when add2 => 
+			OpRegWr <= '0';
+			aluSel <= alu_add;							
+			regRd <= '0';
+			regWr <= '1';
+			regSel <= instrReg(2 downto 0);	
+			next_state <= move3;
+			
+			
+		when sub1 => 
+			OpRegWr <= '0';
+			aluSel <= alu_add;							
+			regRd <= '0';
+			regWr <= '1';
+			regSel <= instrReg(2 downto 0);	
+			next_state <= sub2;
+			
+		when sub2 => 
+			OpRegWr <= '0';
+			aluSel <= alu_add;							
+			regRd <= '0';
+			regWr <= '1';
+			regSel <= instrReg(2 downto 0);	
+			next_state <= move1;
 				
 		when incPc =>
-			NULL;
+			regSel <= regSel;
+			outsel <= '0';
+			
 	end case;
 	end process;
 	
 	
-	controlproc:process(reset,next_state)
+	controlproc:process(reset,next_state,clock)
 	begin
-		if reset = '1' then
+		if reset = '0' then
 			current_state <= execute;
-		else
+		elsif clock'event and clock = '1' then 
 			current_state <= next_state;
 		end if;
 	end process;
