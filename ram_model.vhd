f4f1e3cf268d643df278b815fd7c0cba83173442
lwdoubles <985341219@qq.com>
2018-03-02 11:17:18 +0800

    Add files via upload

--- ram_model.vhd
+++ ram_model.vhd
@@ -8,27 +8,28 @@ entity ram_model is
 			sel:in reg_type;
 	     		reg_wr:in std_logic;
 	     		reg_rd:in std_logic;
-			en:in std_logic;
 			clk:in std_logic;
 			qout: out bit8);
 			
 end ram_model;
 
 architecture rtl of ram_model is
-	type ram_type is array(0 to 3) of bit8;
+	type ram_type is array(0 to 1) of bit8;
 	signal temp_data:bit8;
 begin
-	process(input_data)
+	process(input_data,reg_rd)
 		variable ramdata:ram_type;
 	begin
-		ramdata(conv_integer(sel)) := input_data;	
-		temp_data <= ramdata(conv_integer(sel)) after 1 ns;
+		if reg_wr = '1' then
+			ramdata(conv_integer(sel)) := input_data;			
+			temp_data <= ramdata(conv_integer(sel)) after 1 ns;
+		end if;
 	end process;
 	
-	process(temp_data)
+	process(temp_data,reg_wr)
 	begin
-		if en = '1' then
-			qout <= temp_data after 1 ns;
+		if reg_rd = '1' then
+			qout <= temp_data after 1 ns;	
 		end if;
 	end process;
 	
